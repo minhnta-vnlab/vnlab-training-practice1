36ba4104d180f922be8bf2bc2b6f1484e552f43d
Nguyen Truong Anh Minh <minhnta@vietnamlab.vn>
2024-10-23 10:09:04 +0700

    Remove unused page

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -36,11 +36,11 @@ AppAsset::register($this);
     ]);
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
+    } else {
+        $menuItems[] = ['label'=> 'Profile','url'=> ['/profile']];
     }
 
     echo Nav::widget([
